484f654640bca4079642c5a52956e2748f8234f6
viralvaghela <viralvaghela7109@gmail.com>
2020-10-02 20:51:01 +0530

    card updated &flutter course added

--- learnprogrammingfree/lib/main.dart
+++ learnprogrammingfree/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(MyApp());
 
@@ -34,37 +35,64 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
           appBar: AppBar(
             title: Text("Learn Programming Free"),
           ),
           body: isLoading != true
               ? SingleChildScrollView(
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: data.map((item) {
-                      return Card(
-                        child: Container(
+                      return Container(
                           padding: EdgeInsets.all(10),
-                          
-                          child: Column(
-                            children: [
-                              Text(
-                                "${item['name']}",
-                                style: TextStyle(fontSize: 24),
-                                textAlign: TextAlign.start,
-                              ),
-                              Padding(padding: EdgeInsets.all(10)),
-                              Image.network(item['image']),
-                              Text("${item['description']}"),
-                            ],
-                          ),
-                        ),
-                      );
+                          child: Card(
+                            elevation: 2,
+                            child: Column(
+                              children: [
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text("${item['name']}",
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            color:
+                                                Theme.of(context).primaryColor,
+                                            fontWeight: FontWeight.bold))
+                                  ],
+                                ),
+                                Image.network(
+                                  item['image'],
+                                  fit: BoxFit.cover,
+                                ),
+                                Text("${item['description']}"),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    IconButton(
+                                        icon: Icon(Icons.link),
+                                        onPressed: () {
+                                          launchUrl(item['url']);
+                                        })
+                                  ],
+                                )
+                              ],
+                            ),
+                          ));
                     }).toList(),
                   ),
                 )
-              : Center(child: CircularProgressIndicator()),
-        ));
+              : Center(child: CircularProgressIndicator())),
+    );
+  }
+
+  void launchUrl(url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
