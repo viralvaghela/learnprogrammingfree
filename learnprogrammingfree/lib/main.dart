b2aa529722db86db97cda1350555461f7c06c7fd
sonawane-vivek <viveksonawane600@gmail.com>
2020-10-04 21:05:21 +0530

    added-exit-alert-functionality

--- learnprogrammingfree/lib/main.dart
+++ learnprogrammingfree/lib/main.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
-
+import 'dart:io';
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:url_launcher/url_launcher.dart';
-import 'utils/searchHelper.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,19 +15,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: MyHomePage());
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var listWidth;
   bool isLoading = true;
   List data;
   @override
@@ -48,26 +35,99 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Learn Programming Free"),
-        actions: [
-          GestureDetector(
-            child: Padding(
-              padding: const EdgeInsets.only(right: 20),
-              child: Icon(
-                Icons.search,
-                color: Colors.white,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WillPopScope(
+        // ignore: missing_return
+        onWillPop: (){
+          showModalBottomSheet(context: context, builder: (context){
+            return Container(
+              decoration: BoxDecoration(
+                  color: Colors.transparent
               ),
+              height: 100,
+              width: double.infinity,
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 5,sigmaY: 5),
+                child: Container(
+                  height: 100,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(25),
+                          topRight: Radius.circular(25)
+                      ),
+                      color: Colors.white
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(22, 20, 10, 0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+
+                        Text("Do you really want to exit?",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                              decoration: TextDecoration.none
+                          ),
+                        ),
+                        SizedBox(height: 5,),
+                        Row(
+                          children: [
+                            SizedBox(width: 5,),
+                            FlatButton(
+                              onPressed: (){
+                                Navigator.of(context).pop();
+                              },
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(20))
+                              ),
+                              color: Colors.black,
+                              child: Text(
+                                "Cancel",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                    letterSpacing: 1
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 25,),
+                            FlatButton(
+                              onPressed: (){
+                                exit(0);
+                              },
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(20))
+                              ),
+                              color: Colors.white30,
+                              child: Text(
+                                "Yes",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    letterSpacing: 1,
+                                    color: Colors.black
+                                ),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          } );
+        },
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text("Learn Programming Free"),
             ),
-            onTap: () {
-              showSearch(context: context, delegate: Search(data));
-            },
-          )
-        ],
-      ),
-      body: isLoading != true
-          ? SingleChildScrollView(
+            body: isLoading != true
+                ? SingleChildScrollView(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: data.map((item) {
@@ -77,33 +137,19 @@ class _MyHomePageState extends State<MyHomePage> {
                         elevation: 2,
                         child: Column(
                           children: [
-                            Wrap(
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Padding(
-                                      padding:
-                                          EdgeInsets.only(top: 10, bottom: 10),
-                                      child: Container(
-                                        width:
-                                            (MediaQuery.of(context).size.width -
-                                                    20) *
-                                                0.80,
-                                        child: Text(
-                                          "${item['name']}",
-                                          style: TextStyle(
-                                              fontSize: 20,
-                                              color: Theme.of(context)
-                                                  .primaryColor,
-                                              fontWeight: FontWeight.bold),
-                                          overflow: TextOverflow.clip,
-                                        ),
-                                      ),
-                                    )
-                                  ],
-                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(top: 10),
+                                  child: Text("${item['name']}",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          color: Theme.of(context)
+                                              .primaryColor,
+                                          fontWeight: FontWeight.bold)),
+                                )
                               ],
                             ),
                             Image.network(
@@ -112,9 +158,10 @@ class _MyHomePageState extends State<MyHomePage> {
                             ),
                             Padding(
                               padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                              child: Text("${item['description']}",
-                                  style: TextStyle(),
-                                  overflow: TextOverflow.ellipsis),
+                              child: Text(
+                                "${item['description']}",
+                                style: TextStyle(),
+                              ),
                             ),
                             getTags(item['tags']),
                             Row(
@@ -133,9 +180,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 }).toList(),
               ),
             )
-          : Center(
-              child: CircularProgressIndicator(),
-            ),
+                : Center(child: CircularProgressIndicator())),
+      )
     );
   }
 
@@ -144,7 +190,6 @@ class _MyHomePageState extends State<MyHomePage> {
     List tagsList = list;
     print(tagsList[0]);
     return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: tagsList.map(
